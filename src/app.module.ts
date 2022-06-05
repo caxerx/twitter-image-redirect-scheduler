d92c221af36655fe0d0ec9253ef66432f51e3027
Caxer <webmaster@caxerx.com>
2022-06-06 02:10:25 +0800

    feat: webhook support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegrafModule, TelegrafModuleOptions } from 'nestjs-telegraf';
 import { FileCacheModule } from './modules/file-cache/file-cache.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ScheduledPublishModule } from './modules/scheduled-publish/scheduled-publish.module';
@@ -16,9 +16,25 @@ import { TwitterFetcherModule } from './modules/twitter-fetcher/twitter-fetcher.
     FileCacheModule,
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        let config: TelegrafModuleOptions = {
+          token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
+        };
+
+        if (configService.get<string>('ENV') === 'PROD') {
+          config = {
+            ...config,
+            launchOptions: {
+              webhook: {
+                domain: configService.get<string>('WEBHOOK_URL'),
+                hookPath: '/waifu',
+              },
+            },
+          };
+        }
+
+        return config;
+      },
       inject: [ConfigService],
     }),
     TwitterFetcherModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { getBotToken } from 'nestjs-telegraf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.ENV === 'PROD') {
+    const bot = app.get(getBotToken());
+    app.use(bot.webhookCallback('/waifu'));
+  }
   await app.listen(3000);
 }
 bootstrap();
