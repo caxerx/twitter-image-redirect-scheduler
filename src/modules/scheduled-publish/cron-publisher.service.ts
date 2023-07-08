cbb5c38b1327c1ae6f2a873ffb0264b2d97feab1
Caxer <webmaster@caxerx.com>
2023-07-09 03:02:45 +0900

    fix: twitter api limitation update and cookie login

--- src/modules/scheduled-publish/cron-publisher.service.ts
+++ src/modules/scheduled-publish/cron-publisher.service.ts
@@ -2,10 +2,12 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 import { TelegramBotService } from '../telegram-bot/telegram-bot.service';
+import { TwitterFetcherService } from '../twitter-fetcher/twitter-fetcher.service';
 
 @Injectable()
 export class CronPublisherService {
   constructor(
+    private twitterFetcher: TwitterFetcherService,
     private telegramBot: TelegramBotService,
     private config: ConfigService,
   ) {}
@@ -13,8 +15,19 @@ export class CronPublisherService {
   logger = new Logger(CronPublisherService.name);
 
   @Cron('0 0 */3 * * *')
-  handleCron() {
+  publishTweet() {
     const adminUser = this.config.get<string>('ADMIN_USER');
     this.telegramBot.publishScheduled(adminUser);
   }
+
+  // @Cron('0 */1 * * * *')
+  // async fetchThings() {
+  //   const tweets = this.twitterFetcher.fetchLastRetweets();
+
+  //   for await (const tweet of tweets) {
+  //     console.log(tweet);
+  //   }
+
+  //   console.log('Done');
+  // }
 }
--- src/modules/twitter-fetcher/twitter-fetcher.service.ts
+++ src/modules/twitter-fetcher/twitter-fetcher.service.ts
@@ -4,22 +4,91 @@ import { GentleError } from 'src/models/GentleError';
 import { FileCacheService } from '../file-cache/file-cache.service';
 import { Scraper } from '@the-convocation/twitter-scraper';
 import { BufferFile } from '../file-cache/file-cache.types';
+import * as fs from 'fs';
+import * as tough from 'tough-cookie';
 
 @Injectable()
 export class TwitterFetcherService {
   private scraper: Scraper;
-  constructor(config: ConfigService, private fileCache: FileCacheService) {
+  // private lastRetweetedId: string;
+
+  constructor(
+    private config: ConfigService,
+    private fileCache: FileCacheService,
+  ) {
     this.scraper = new Scraper();
-    this.scraper.login(
-      config.get('TWITTER_USERNAME'),
-      config.get('TWITTER_PASSWORD'),
-    );
+    this.login();
+  }
+
+  private async login() {
+    if (fs.existsSync('cookie.json')) {
+      this.logger.log('Logging in Twitter using cookie');
+      await this.scraper.setCookies(
+        JSON.parse(fs.readFileSync('cookie.json').toString()).map((i) =>
+          tough.Cookie.fromJSON(i),
+        ),
+      );
+    } else {
+      this.logger.log('Logging in Twitter using username and password');
+      await this.scraper.login(
+        this.config.get('TWITTER_USERNAME'),
+        this.config.get('TWITTER_PASSWORD'),
+      );
+
+      fs.writeFileSync(
+        'cookie.json',
+        JSON.stringify(await this.scraper.getCookies()),
+      );
+    }
+
+    if (await this.scraper.isLoggedIn()) {
+      this.logger.log('Logged in successfully');
+      // setTimeout(() => {
+      //   this.logLastRetweets();
+      // }, 1000);
+    } else {
+      this.logger.error('Failed to login');
+    }
   }
 
   private logger = new Logger(TwitterFetcherService.name);
 
+  // async logLastRetweets() {
+  //   const lastRetweet = this.scraper.getTweets(
+  //     this.config.get('TWITTER_RETWEET_TRACKER_USERNAME'),
+  //   );
+
+  //   for await (const tweet of lastRetweet) {
+  //     console.log(tweet);
+  //     if (tweet.isRetweet) {
+  //       console.log(tweet);
+  //       this.lastRetweetedId = tweet.id;
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // async *fetchLastRetweets() {
+  //   if (!this.lastRetweetedId) {
+  //     return;
+  //   }
+  //   const tweets = this.scraper.getTweets(
+  //     this.config.get('TWITTER_USERNAME'),
+  //     15,
+  //   );
+  //   for await (const tweet of tweets) {
+  //     if (tweet.isRetweet) {
+  //       if (tweet.id === this.lastRetweetedId) {
+  //         break;
+  //       }
+  //       this.lastRetweetedId = tweet.id;
+  //       yield tweet;
+  //     }
+  //   }
+  // }
+
   async getTweetImages(tweetId: string): Promise<string[]> {
-    const tweet = await this.scraper.getTweet(tweetId, false);
+    const tweet = await this.scraper.getTweet(tweetId);
 
     if (tweet.photos?.length <= 0) {
       return [];
@@ -29,7 +98,7 @@ export class TwitterFetcherService {
   }
 
   async getTweetVideos(tweetId: string): Promise<string[]> {
-    const tweet = await this.scraper.getTweet(tweetId, false);
+    const tweet = await this.scraper.getTweet(tweetId);
 
     if (tweet.videos?.length <= 0) {
       return [];
